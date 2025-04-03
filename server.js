f51d0c59c85a9fd19c958d7a6a984d3f9fd8bf28
Gregor Cool <willytk249@gmail.com>
2025-04-03 09:40:38 -0400

    class changes

--- server.js
+++ server.js
@@ -10,10 +10,56 @@ app.get("/",(req, res)=>{
 });
 
 let houses = [
-    {name:"Farmhouse"},
-    {name:"Cool House"}
+    {
+        "_id": "1",
+        "name": "Mower",
+        "description": "Useful for cutting the grass when it gets too tall.",
+        "price": "129.99",
+        "rating": "4.2",
+        "img1": "https://machine8822.github.io/project/part6/images/mower.png",
+        "img2": "https://machine8822.github.io/project/part6/images/placeholder.jpg"
+    },
+    {
+        "_id": "2",
+        "name": "Rake",
+        "description": "Useful for moving leaves into a pile.",
+        "price": "9.99",
+        "rating": "3.4",
+        "img1": "https://machine8822.github.io/project/part6/images/rake.png",
+        "img2": "https://machine8822.github.io/project/part6/images/placeholder.jpg"
+    },
+    {
+        "_id": "3",
+        "name": "Edger",
+        "description": "Useful for trimming up the edge of the grass.",
+        "price": "99.99",
+        "rating": "4.4",
+        "img1": "https://machine8822.github.io/project/part6/images/edger.png",
+        "img2": "https://machine8822.github.io/project/part6/images/placeholder.jpg"
+    },
+    {
+        "_id": "4",
+        "name": "Leaf Blower",
+        "description": "Useful for blowing leaves wherever you need to.",
+        "price": "38.99",
+        "rating": "3.3",
+        "img1": "https://machine8822.github.io/project/part6/images/leafblower.png",
+        "img2": "https://machine8822.github.io/project/part6/images/placeholder.jpg"
+    },
+    {
+        "_id": "5",
+        "name": "Spreader",
+        "description": "Useful for spreading pesticide and other chemicals over a wide area.",
+        "price": "49.99",
+        "rating": "2.7",
+        "img1": "https://machine8822.github.io/project/part6/images/spreader.png",
+        "img2": "https://machine8822.github.io/project/part6/images/placeholder.jpg"
+    }
 ];
 
+app.get("/api/houses", (req, res)=>{
+    res.send(houses);
+});
 
 app.listen(3001, ()=>{
     console.log("I'm listening");
